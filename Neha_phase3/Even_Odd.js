0910d3f1d3404861cab8e2e39be1ab593780ef45
Nehano8 <naveenneha44@gmail.com>
2026-04-25 14:22:26 +0530

    Add files via upload

---
+++ Neha_phase3/Even_Odd.js
@@ -0,0 +1,14 @@
+function oddeven(num) 
+{
+    if (num % 2 === 0) 
+    {
+        return "Even";
+    } 
+    else 
+    {
+        return "Odd";
+    }
+}
+
+console.log(oddeven(19));
+console.log(oddeven(2));
